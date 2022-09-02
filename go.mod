ca46d83fd425aead9dde1c54547737680e89091f
tiyan-attirmidzi <tiyanattirmidzi20@gmail.com>
2022-09-02 11:05:51 +0700

    mod release

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/onggolt-dev/learn-go-mod
+
+go 1.18
---
+++ hello.go
@@ -0,0 +1,7 @@
+package hello
+
+import "fmt"
+
+func SayHello(name string) string {
+	return fmt.Sprintf("Hello Bro %s!", name)
+}
